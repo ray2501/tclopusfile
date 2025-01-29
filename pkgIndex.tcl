3590fe36663f57ee785eb51891caf86fe93c8bd8
Danilo Chang <ray2501@gmail.com>
2025-01-29 11:35:30 +0800

    Bump version to 0.6

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded opusfile 0.5 \
-	    [list load [file join $dir libtcl9opusfile0.5.so] [string totitle opusfile]]
+    package ifneeded opusfile 0.6 \
+	    [list load [file join $dir libtcl9opusfile0.6.so] [string totitle opusfile]]
 } else {
-    package ifneeded opusfile 0.5 \
-	    [list load [file join $dir libopusfile0.5.so] [string totitle opusfile]]
+    package ifneeded opusfile 0.6 \
+	    [list load [file join $dir libopusfile0.6.so] [string totitle opusfile]]
 }
